2417dd488db5a966773305b0ac10a9d65d428b48
yann300 <yann.levreau@gmail.com>
2016-11-22 16:07:44 +0100

    add solidity state dropdownpanel

---
+++ src/ui/SolidityState.js
@@ -0,0 +1,67 @@
+'use strict'
+var DropdownPanel = require('./DropdownPanel')
+var traceHelper = require('../helpers/traceHelper')
+var stateDecoder = require('../solidity/stateDecoder')
+var yo = require('yo-yo')
+
+function SolidityState (_parent, _traceManager, _codeManager) {
+  this.parent = _parent
+  this.traceManager = _traceManager
+  this.codeManager = _codeManager
+  this.basicPanel = new DropdownPanel('Solidity State')
+  this.init()
+}
+
+SolidityState.prototype.render = function () {
+  return yo`<div id='soliditypanel' >${this.basicPanel.render()}</div>`
+}
+
+SolidityState.prototype.init = function () {
+  var self = this
+  this.parent.event.register('indexChanged', this, function (index) {
+    if (index < 0) return
+    if (self.parent.currentStepIndex !== index) return
+    if (!this.parent.contractsDetail || !this.parent.sources) {
+      return self.basicPanel.update({info: 'no source has been specified'})
+    }
+
+    self.traceManager.getStorageAt(index, null, function (error, storage) {
+      if (error) {
+        self.basicPanel.update({ info: error })
+      } else {
+        self.traceManager.getCurrentCalledAddressAt(index, function (error, address) {
+          if (error) {
+            self.basicPanel.update({ info: error })
+          } else {
+            self.codeManager.getCode(address, function (error, code) {
+              if (error) {
+                self.basicPanel.update({ info: error })
+              } else {
+                var contractName = contractNameFromCode(self.parent.contractsDetail, code.bytecode, address)
+                if (!contractName) {
+                  self.basicPanel.update({ info: 'could not find compiled contract with address ' + address })
+                } else {
+                  var state = stateDecoder.solidityState(storage, self.parent.sources, contractName)
+                  self.basicPanel.update(state)
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+  })
+}
+
+function contractNameFromCode (contracts, code, address) {
+  var isCreation = traceHelper.isContractCreation(address)
+  var byteProp = isCreation ? 'bytecode' : 'runtimeBytecode'
+  for (var k in contracts) {
+    if ('0x' + contracts[k][byteProp] === code) {
+      return k
+    }
+  }
+  return null
+}
+
+module.exports = SolidityState
