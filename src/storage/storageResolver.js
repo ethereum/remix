126e335d521a69eaf9ed4f3160ef0ab1f5257fc9
yann300 <yann.levreau@gmail.com>
2017-03-14 11:21:47 +0100

    storageResolver

--- src/storage/storageResolver.js
+++ src/storage/storageResolver.js
@@ -1,92 +1,75 @@
 'use strict'
 var traceHelper = require('../helpers/traceHelper')
+var helper = require('../helpers/util')
 var util = require('../helpers/global')
 
 class StorageResolver {
-  constructor (_debugger) {
+  constructor (_debugger, _traceManager) {
     this.debugger = _debugger
+    this.traceManager = _traceManager
     this.clear()
     _debugger.event.register('newTraceLoaded', () => {
       this.clear()
     })
   }
 
+  /**
+    * return the storage for the current context (address and vm trace index)
+    * by default now returns the range 0 => 1000
+    *
+    * @param {Function} - callback - contains a map: [hashedKey] = {key, hashedKey, value}
+    */
   storageRange (callback) {
-    resolveAddress(this.debugger.traceManager, this.debugger.currentStepIndex, (error, address) => {
+    storageRangeInternal(this, '0x0', 1000, callback)
+  }
+
+  /**
+    * return a slot value for the current context (address and vm trace index)
+    *
+    * @param {Function} - callback - {key, hashedKey, value} -
+    */
+  storageSlot (slot, callback) {
+    storageRangeInternal(this, slot, 100, function (error, storage) {
       if (error) {
         callback(error)
       } else {
-        if (traceHelper.isContractCreation(address)) {
-          callback(null, {})
-        } else {
-          if (util.web3.debug.storageRangeAt) {
-            if (this.isComplete(address)) {
-              callback(null, this.fromCache(address))
-            } else {
-              var start = '0x0'
-              var maxSize = 10000
-              // we just load the entire storage here. that stuff is used by FullStoragesChanges.js.
-              // assume 10000 is enough for now
-              // TODO: improve that later
-              storageRangeAtInternal(this.debugger.tx, address, start, maxSize, (error, result) => {
-                console.log('requesting storageRange ' + result)
-                if (error) {
-                  callback(error)
-                } else {
-                  this.toCache(address, result, true)
-                  callback(null, result)
-                }
-              })
-            }
-          } else {
-            callback('no storageRangeAt endpoint found')
-          }
-        }
+        var hashed = helper.sha3(slot)
+        callback(null, storage[hashed] !== undefined ? storage[hashed] : null)
       }
     })
   }
 
-  storageSlotValue (slot, callback) {
-    resolveAddress(this.debugger.traceManager, this.debugger.currentStepIndex, (error, address) => {
-      if (error) {
-        callback(error)
-      } else {
-        var slotValue = this.fromCache(address, slot)
-        slotValue = slotValue.value
-        if (slotValue) {
-          callback(null, slotValue)
-        } else {
-          // we load the asked slot and the next 1000 slots
-
-          storageRangeAtInternal(this.debugger.tx, address, slot, 1000, (error, result) => {
-            console.log('requesting storageSlotValue ' + slot + ' ' + result)
-            var slotValue = result[0].value
-            if (error) {
-              callback(error)
-            } else {
-              this.toCache(address, result)
-              callback(null, {
-                key: slot,
-                slotValue: slotValue
-              })
-            }
-          })
-        }
-      }
-    })
-  }
-
+  /**
+    * return True if the storage at @arg address is complete
+    *
+    * @param {String} address  - contract address
+    * @return {Bool} - return True if the storage at @arg address is complete
+    */
   isComplete (address) {
     return this.storageByAddress[address] && this.storageByAddress[address].complete
   }
 
-  fromCache (address, slot) {
+  /**
+    * retrieve the storage from the cache. if @arg slot is defined, return only the desired slot, if not return the entire known storage
+    *
+    * @param {String} address  - contract address
+    * @param {String} slotKey  - key of the value to return
+    * @return {String} - either the entire known storage or a single value
+    */
+  fromCache (address, slotKey) {
     if (!this.storageByAddress[address]) {
       return null
     }
-    return slot ? this.storageByAddress[address].storage[slot] : this.storageByAddress[address].storage
+    return slotKey ? this.storageByAddress[address].storage[slotKey] : this.storageByAddress[address].storage
   }
 
+  /**
+    * store the result of `storageRangeAtInternal`
+    *
+    * @param {String} address  - contract address
+    * @param {Object} storage  - result of `storageRangeAtInternal`, contains {key, hashedKey, value}
+    * @param {Bool} complete  - True if the storage is complete
+    */
   toCache (address, storage, complete) {
     if (!this.storageByAddress[address]) {
       this.storageByAddress[address] = {}
@@ -97,13 +80,29 @@ class StorageResolver {
     }
   }
 
+  /**
+    * clear the cache
+    *
+    */
   clear () {
     this.storageByAddress = {}
   }
+
+  /**
+    * resolve the storage to the specified execution step @arg index. uses the traceManager.resolveStorage
+    *
+    * @param {Int} index  - execution step index
+    * @param {String} address  - contract address
+    * @param {Map} storage  - Map of the known storage
+    * @return {Map} - The storage resolved to the given exection point
+    */
+  resolveStorage (address, storage, callback) {
+    this.traceManager.resolveStorage(this.debugger.currentStepIndex, address, storage, callback)
+  }
 }
 
-function resolveAddress (traceManager, currentStep, callback) {
-  traceManager.getCurrentCalledAddressAt(currentStep, (error, result) => {
+function resolveAddress (self, callback) {
+  self.traceManager.getCurrentCalledAddressAt(self.debugger.currentStepIndex, (error, result) => {
     if (error) {
       callback(error)
     } else {
@@ -122,11 +121,41 @@ function storageRangeAtInternal (tx, address, start, maxSize, callback) {
       if (error) {
         callback(error)
       } else if (result.storage) {
-        callback(null, result.storage)
+        callback(null, result.storage, result.complete)
       } else {
-        callback('storage has not been provided')
+        callback('the storage has not been provided')
       }
     })
+}
+
+function storageRangeInternal (self, start, maxSize, callback) {
+  resolveAddress(self, (error, address) => {
+    if (error) {
+      callback(error)
+    } else {
+      if (traceHelper.isContractCreation(address)) {
+        callback(null, {})
+      } else {
+        if (!util.web3.debug.storageRangeAt) {
+          callback('no storageRangeAt endpoint found')
+        } else {
+          if (self.isComplete(address)) {
+            var cached = self.fromCache(address)
+            self.resolveStorage(address, cached, callback)
+          } else {
+            storageRangeAtInternal(self.debugger.tx, address, start, maxSize, (error, storage, complete) => {
+              if (error) {
+                callback(error)
+              } else {
+                self.toCache(address, storage, complete)
+                self.resolveStorage(address, storage, callback)
+              }
+            })
+          }
+        }
+      }
+    }
+  })
 }
 
 module.exports = StorageResolver
