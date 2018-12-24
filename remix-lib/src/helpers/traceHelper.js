d9eae2306ae49144d9381dcbf7971598832b2388
Iuri Matias <iuri.matias@gmail.com>
2018-12-24 11:07:24 -0500

    make linter happy

--- remix-lib/src/helpers/traceHelper.js
+++ remix-lib/src/helpers/traceHelper.js
@@ -53,7 +53,7 @@ module.exports = {
     // if stack empty => this is not a precompiled contract
     var step = trace[index]
     if (!this.isCallInstruction(step)) {
-      return false;
+      return false
     }
     return index + 1 < trace.length && trace[index + 1].stack.length !== 0
   },
