84cf1cadd9f3bbeb10ef8cfac3661dc664456b02
Omkara <0mkar@protonmail.com>
2019-02-28 13:42:43 +0530

    passing tests

--- remix-astwalker/src/types.ts
+++ remix-astwalker/src/types.ts
@@ -2,3 +2,21 @@ export interface Node {
   ast: any | null,
   source: string | null
 }
+
+export interface AstNode {
+  id: number
+  name: string
+  src: string
+  children: Array<AstNode>
+  attributes: AstNodeAtt
+}
+
+export interface AstNodeAtt {
+  operator: string
+  string: null
+  type: string
+  value: string
+  constant: boolean
+  name: string
+  public: boolean
+}
