d2c7d28a3b4913ae0453522f6cbc91589ea3e27a
Ayanrocks <ayanb1999@gmail.com>
2019-03-23 22:05:36 +0530

    Tests Passed for AstNodeLegacy

--- remix-astwalker/src/types.ts
+++ remix-astwalker/src/types.ts
@@ -1,25 +1,43 @@
 export interface Node {
-  ast: any | null,
-  source: string | null
+  ast?: AstNode;
+  legacyAST?: AstNodeLegacy;
+  source?: string | null;
+  id: number;
 }
 
 export interface AstNode {
+  absolutePath?: string;
+  exportedSymbols?: Object;
+  id: number;
+  nodeType: string;
+  nodes?: Array<AstNode>;
+  src: string;
+  literals?: Array<string>;
+  file?: string;
+  scope?: number;
+  sourceUnit?: number;
+  symbolAliases?: Array<string>;
+  [x: string]: any;
 }
 
 export interface AstNodeLegacy {
-  id: number
-  name: string
-  src: string
-  children: Array<AstNode>
-  attributes: AstNodeAtt
+  id: number;
+  name: string;
+  src: string;
+  children?: Array<AstNodeLegacy>;
+  attributes?: AstNodeAtt;
 }
 
 export interface AstNodeAtt {
-  operator: string
-  string: null
-  type: string
-  value: string
-  constant: boolean
-  name: string
-  public: boolean
+  operator?: string;
+  string?: null;
+  type?: string;
+  value?: string;
+  constant?: boolean;
+  name?: string;
+  public?: boolean;
+  exportedSymbols?: Object;
+  argumentTypes?: null;
+  absolutePath?: string;
+  [x: string]: any;
 }
