fa0c71c4aa16eae2a5e3f850f13a27893800f0ec
yann300 <yann.levreau@gmail.com>
2019-05-13 08:36:37 +0200

    refactor remix-solidity

--- remix-solidity/src/compiler/compiler.js
+++ remix-solidity/src/compiler/compiler.js
@@ -15,169 +15,157 @@ var txHelper = require('./txHelper')
 /*
   trigger compilationFinished, compilerLoaded, compilationStarted, compilationDuration
 */
-function Compiler (handleImportCall) {
-  var self = this
-  this.event = new EventManager()
-
-  var compileJSON
-
-  var worker = null
-
-  var currentVersion
-
-  var optimize = false
-
-  this.setOptimize = function (_optimize) {
-    optimize = _optimize
+export class Compiler {
+  constructor (handleImportCall) {
+    this.event = new EventManager()
+    this.state = {
+      compileJSON: null,
+      worker: null,
+      handleImportCall,
+      currentVersion: null,
+      lastCompilationResult: {
+        data: null,
+        source: null
+      }
+    }
+    this._trackCompilationDuration()
   }
 
-  var compilationStartTime = null
-  this.event.register('compilationFinished', (success, data, source) => {
-    if (success && compilationStartTime) {
-      this.event.trigger('compilationDuration', [(new Date().getTime()) - compilationStartTime])
-    }
-    compilationStartTime = null
-  })
+  compile (source, optimize) {
+    this.event.trigger('compilationStarted', [])
+    this._internalCompile(source, optimize)
+  }
 
-  this.event.register('compilationStarted', () => {
-    compilationStartTime = new Date().getTime()
-  })
+  _onCompilerLoaded (version) {
+    this.state.currentVersion = version
+    this.event.trigger('compilerLoaded', [version])
+  }
 
-  var internalCompile = function (files, target, missingInputs) {
-    gatherImports(files, target, missingInputs, function (error, input) {
-      if (error) {
-        self.lastCompilationResult = null
-        self.event.trigger('compilationFinished', [false, {'error': { formattedMessage: error, severity: 'error' }}, files])
-      } else {
-        compileJSON(input, optimize ? 1 : 0)
+  _trackCompilationDuration () {
+    var compilationStartTime = null
+    this.event.register('compilationFinished', (success, data, source) => {
+      if (success && compilationStartTime) {
+        this.event.trigger('compilationDuration', [(new Date().getTime()) - compilationStartTime])
       }
+      compilationStartTime = null
+    })
+
+    this.event.register('compilationStarted', () => {
+      compilationStartTime = new Date().getTime()
     })
   }
 
-  var compile = function (files, target) {
-    self.event.trigger('compilationStarted', [])
-    internalCompile(files, target)
-  }
-  this.compile = compile
-
-  function setCompileJSON (_compileJSON) {
-    compileJSON = _compileJSON
-  }
-  this.setCompileJSON = setCompileJSON // this is exposed for testing
-
-  function onCompilerLoaded (version) {
-    currentVersion = version
-    self.event.trigger('compilerLoaded', [version])
+  _internalCompile (source, optimize) {
+    if (!this.state.compileJSON) throw new Error('no compiler loaded')
+    this.state.compileJSON(source, optimize ? 1 : 0)
   }
 
-  function onInternalCompilerLoaded () {
-    if (worker === null) {
-      var compiler
-      var userAgent = (typeof (navigator) !== 'undefined') && navigator.userAgent ? navigator.userAgent.toLowerCase() : '-'
-      if (typeof (window) === 'undefined') {
-        compiler = require('solc')
-      } else {
-        compiler = solc(window.Module)
+  loadVersion (usingWorker, url) {
+    // Set a safe fallback until the new one is loaded
+    this.state.compileJSON = () => {
+      this._compilationFinished({ error: { formattedMessage: 'Compiler not yet loaded.' } })
+    }
+    console.log('Loading ' + url + ' ' + (usingWorker ? 'with worker' : 'without worker'))
+    this.event.trigger('loadingCompiler', [url, usingWorker])
+
+    if (usingWorker) {
+      this._loadWorker(url)
+    } else {
+      this._loadInternal(url)
+    }
+  }
+
+  _loadWorker (url) {
+    let self = this
+    if (this.state.worker !== null) {
+      this.state.worker.terminate()
+    }
+    this.state.worker.workerer = webworkify(require('./compiler-worker.js'))
+    var jobs = []
+    this.state.worker.addEventListener('message', function (msg) {
+      var data = msg.data
+      switch (data.cmd) {
+        case 'versionLoaded':
+          self._onCompilerLoaded(data.data)
+          break
+        case 'compiled':
+          var result
+          try {
+            result = JSON.parse(data.data)
+          } catch (exception) {
+            result = { 'error': 'Invalid JSON output from the compiler: ' + exception }
+          }
+          var sources = {}
+          if (data.job in jobs !== undefined) {
+            sources = jobs[data.job].sources
+            delete jobs[data.job]
+          }
+          self._compilationFinished(result, data.missingInputs, sources)
+          break
+      }
+    })
+    this.state.worker.onerror = function (msg) {
+      self._compilationFinished({ error: 'Worker error: ' + msg.data })
+    }
+    this.state.worker.addEventListener('error', function (msg) {
+      self._compilationFinished({ error: 'Worker error: ' + msg.data })
+    })
+    this.state.compileJSON = (source, optimize) => {
+      jobs.push({sources: source})
+      this.state.worker.postMessage({cmd: 'compile', job: jobs.length - 1, input: compilerInput(source.sources, {optimize: optimize, target: source.target})})
+    }
+    this.state.worker.postMessage({cmd: 'loadVersion', data: url})
+  }
+
+  _loadInternal (url) {
+    let self = this
+    delete window.Module
+    // NOTE: workaround some browsers?
+    window.Module = undefined
+
+    var newScript = document.createElement('script')
+    newScript.type = 'text/javascript'
+    newScript.src = url
+    document.getElementsByTagName('head')[0].appendChild(newScript)
+    var check = window.setInterval(function () {
+      if (!window.Module) {
+        return
+      }
+      window.clearInterval(check)
+      self._onInternalCompilerLoaded()
+    }, 200)
+  }
+
+  _onInternalCompilerLoaded () {
+    var compiler
+    if (typeof (window) === 'undefined') {
+      compiler = require('solc')
+    } else {
+      compiler = solc(window.Module)
+    }
+
+    this.state.compileJSON = (source, optimize) => {
+      var missingInputs = []
+      var missingInputsCallback = function (path) {
+        missingInputs.push(path)
+        return { error: 'Deferred import' }
       }
 
-      compileJSON = function (source, optimize, cb) {
-        var missingInputs = []
-        var missingInputsCallback = function (path) {
-          missingInputs.push(path)
-          return { error: 'Deferred import' }
-        }
-
-        var result
-        try {
-          var input = compilerInput(source.sources, {optimize: optimize, target: source.target})
-          result = compiler.compile(input, missingInputsCallback)
-          result = JSON.parse(result)
-        } catch (exception) {
-          result = { error: { formattedMessage: 'Uncaught JavaScript exception:\n' + exception, severity: 'error', mode: 'panic' } }
-        }
-
-        compilationFinished(result, missingInputs, source)
+      var result
+      try {
+        var input = compilerInput(source.sources, {optimize: optimize, target: source.target})
+        result = compiler.compile(input, missingInputsCallback)
+        result = JSON.parse(result)
+      } catch (exception) {
+        result = { error: { formattedMessage: 'Uncaught JavaScript exception:\n' + exception, severity: 'error', mode: 'panic' } }
       }
-      onCompilerLoaded(compiler.version())
+
+      this._compilationFinished(result, missingInputs, source)
     }
-  }
-  // exposed for use in node
-  this.onInternalCompilerLoaded = onInternalCompilerLoaded
-
-  this.lastCompilationResult = {
-    data: null,
-    source: null
+    this._onCompilerLoaded(compiler.version())
   }
 
-  /**
-    * return the contract obj of the given @arg name. Uses last compilation result.
-    * return null if not found
-    * @param {String} name    - contract name
-    * @returns contract obj and associated file: { contract, file } or null
-    */
-  this.getContract = (name) => {
-    if (this.lastCompilationResult.data && this.lastCompilationResult.data.contracts) {
-      return txHelper.getContract(name, this.lastCompilationResult.data.contracts)
-    }
-    return null
-  }
-
-  /**
-    * call the given @arg cb (function) for all the contracts. Uses last compilation result
-    * @param {Function} cb    - callback
-    */
-  this.visitContracts = (cb) => {
-    if (this.lastCompilationResult.data && this.lastCompilationResult.data.contracts) {
-      return txHelper.visitContracts(this.lastCompilationResult.data.contracts, cb)
-    }
-    return null
-  }
-
-  /**
-    * return the compiled contracts from the last compilation result
-    * @return {Object}     - contracts
-    */
-  this.getContracts = () => {
-    if (this.lastCompilationResult.data && this.lastCompilationResult.data.contracts) {
-      return this.lastCompilationResult.data.contracts
-    }
-    return null
-  }
-
-   /**
-    * return the sources from the last compilation result
-    * @param {Object} cb    - map of sources
-    */
-  this.getSources = () => {
-    if (this.lastCompilationResult.source) {
-      return this.lastCompilationResult.source.sources
-    }
-    return null
-  }
-
-  /**
-    * return the sources @arg fileName from the last compilation result
-    * @param {Object} cb    - map of sources
-    */
-  this.getSource = (fileName) => {
-    if (this.lastCompilationResult.source) {
-      return this.lastCompilationResult.source.sources[fileName]
-    }
-    return null
-  }
-
-  /**
-    * return the source from the last compilation result that has the given index. null if source not found
-    * @param {Int} index    - index of the source
-    */
-  this.getSourceName = (index) => {
-    if (this.lastCompilationResult.data && this.lastCompilationResult.data.sources) {
-      return Object.keys(this.lastCompilationResult.data.sources)[index]
-    }
-    return null
-  }
-
-  function compilationFinished (data, missingInputs, source) {
+  _compilationFinished (data, missingInputs, source) {
     var noFatalErrors = true // ie warnings are ok
 
     function isValidError (error) {
@@ -207,151 +195,90 @@ function Compiler (handleImportCall) {
 
     if (!noFatalErrors) {
       // There are fatal errors - abort here
-      self.lastCompilationResult = null
-      self.event.trigger('compilationFinished', [false, data, source])
+      this.lastCompilationResult = null
+      this.event.trigger('compilationFinished', [false, data, source])
     } else if (missingInputs !== undefined && missingInputs.length > 0) {
-      // try compiling again with the new set of inputs
-
-      internalCompile(source.sources, source.target, missingInputs)
+      this.event.trigger('compilationFinished', [false, data, source])
     } else {
-      data = updateInterface(data)
+      data = this.updateInterface(data)
 
-      self.lastCompilationResult = {
+      this.state.lastCompilationResult = {
         data: data,
         source: source
       }
-      self.event.trigger('compilationFinished', [true, data, source])
+      this.event.trigger('compilationFinished', [true, data, source])
     }
   }
 
-  // TODO: needs to be changed to be more node friendly
-  this.loadVersion = function (usingWorker, url) {
-    console.log('Loading ' + url + ' ' + (usingWorker ? 'with worker' : 'without worker'))
-    self.event.trigger('loadingCompiler', [url, usingWorker])
-
-    if (usingWorker) {
-      loadWorker(url)
-    } else {
-      loadInternal(url)
+  /**
+    * return the contract obj of the given @arg name. Uses last compilation result.
+    * return null if not found
+    * @param {String} name    - contract name
+    * @returns contract obj and associated file: { contract, file } or null
+    */
+  getContract (name) {
+    if (this.lastCompilationResult.data && this.lastCompilationResult.data.contracts) {
+      return txHelper.getContract(name, this.lastCompilationResult.data.contracts)
     }
+    return null
   }
 
-  function loadInternal (url) {
-    delete window.Module
-    // NOTE: workaround some browsers?
-    window.Module = undefined
-
-    // Set a safe fallback until the new one is loaded
-    setCompileJSON(function (source, optimize) {
-      compilationFinished({ error: { formattedMessage: 'Compiler not yet loaded.' } })
-    })
-
-    var newScript = document.createElement('script')
-    newScript.type = 'text/javascript'
-    newScript.src = url
-    document.getElementsByTagName('head')[0].appendChild(newScript)
-    var check = window.setInterval(function () {
-      if (!window.Module) {
-        return
-      }
-      window.clearInterval(check)
-      onInternalCompilerLoaded()
-    }, 200)
+  /**
+    * call the given @arg cb (function) for all the contracts. Uses last compilation result
+    * @param {Function} cb    - callback
+    */
+  visitContracts (cb) {
+    if (this.lastCompilationResult.data && this.lastCompilationResult.data.contracts) {
+      return txHelper.visitContracts(this.lastCompilationResult.data.contracts, cb)
+    }
+    return null
   }
 
-  function loadWorker (url) {
-    if (worker !== null) {
-      worker.terminate()
+  /**
+    * return the compiled contracts from the last compilation result
+    * @return {Object}     - contracts
+    */
+  getContracts () {
+    if (this.lastCompilationResult.data && this.lastCompilationResult.data.contracts) {
+      return this.lastCompilationResult.data.contracts
     }
-    worker = webworkify(require('./compiler-worker.js'))
-    var jobs = []
-    worker.addEventListener('message', function (msg) {
-      var data = msg.data
-      switch (data.cmd) {
-        case 'versionLoaded':
-          onCompilerLoaded(data.data)
-          break
-        case 'compiled':
-          var result
-          try {
-            result = JSON.parse(data.data)
-          } catch (exception) {
-            result = { 'error': 'Invalid JSON output from the compiler: ' + exception }
-          }
-          var sources = {}
-          if (data.job in jobs !== undefined) {
-            sources = jobs[data.job].sources
-            delete jobs[data.job]
-          }
-          compilationFinished(result, data.missingInputs, sources)
-          break
-      }
-    })
-    worker.onerror = function (msg) {
-      compilationFinished({ error: 'Worker error: ' + msg.data })
-    }
-    worker.addEventListener('error', function (msg) {
-      compilationFinished({ error: 'Worker error: ' + msg.data })
-    })
-    compileJSON = function (source, optimize) {
-      jobs.push({sources: source})
-      worker.postMessage({cmd: 'compile', job: jobs.length - 1, input: compilerInput(source.sources, {optimize: optimize, target: source.target})})
-    }
-    worker.postMessage({cmd: 'loadVersion', data: url})
+    return null
   }
 
-  function gatherImports (files, target, importHints, cb) {
-    importHints = importHints || []
-
-    // FIXME: This will only match imports if the file begins with one.
-    //        It should tokenize by lines and check each.
-    // eslint-disable-next-line no-useless-escape
-    var importRegex = /^\s*import\s*[\'\"]([^\'\"]+)[\'\"];/g
-
-    for (var fileName in files) {
-      var match
-      while ((match = importRegex.exec(files[fileName].content))) {
-        var importFilePath = match[1]
-        if (importFilePath.startsWith('./')) {
-          var path = /(.*\/).*/.exec(fileName)
-          if (path !== null) {
-            importFilePath = importFilePath.replace('./', path[1])
-          } else {
-            importFilePath = importFilePath.slice(2)
-          }
-        }
-
-        // FIXME: should be using includes or sets, but there's also browser compatibility..
-        if (importHints.indexOf(importFilePath) === -1) {
-          importHints.push(importFilePath)
-        }
-      }
+   /**
+    * return the sources from the last compilation result
+    * @param {Object} cb    - map of sources
+    */
+  getSources () {
+    if (this.lastCompilationResult.source) {
+      return this.lastCompilationResult.source.sources
     }
-
-    while (importHints.length > 0) {
-      var m = importHints.pop()
-      if (m in files) {
-        continue
-      }
-
-      if (handleImportCall) {
-        handleImportCall(m, function (err, content) {
-          if (err) {
-            cb(err)
-          } else {
-            files[m] = { content }
-            gatherImports(files, target, importHints, cb)
-          }
-        })
-      }
-
-      return
-    }
-
-    cb(null, { 'sources': files, 'target': target })
+    return null
   }
 
-  function truncateVersion (version) {
+  /**
+    * return the sources @arg fileName from the last compilation result
+    * @param {Object} cb    - map of sources
+    */
+  getSource (fileName) {
+    if (this.lastCompilationResult.source) {
+      return this.lastCompilationResult.source.sources[fileName]
+    }
+    return null
+  }
+
+  /**
+    * return the source from the last compilation result that has the given index. null if source not found
+    * @param {Int} index    - index of the source
+    */
+  getSourceName (index) {
+    if (this.lastCompilationResult.data && this.lastCompilationResult.data.sources) {
+      return Object.keys(this.lastCompilationResult.data.sources)[index]
+    }
+    return null
+  }
+
+  truncateVersion (version) {
     var tmp = /^(\d+.\d+.\d+)/.exec(version)
     if (tmp) {
       return tmp[1]
@@ -359,12 +286,10 @@ function Compiler (handleImportCall) {
     return version
   }
 
-  function updateInterface (data) {
+  updateInterface (data) {
     txHelper.visitContracts(data.contracts, (contract) => {
-      data.contracts[contract.file][contract.name].abi = solcABI.update(truncateVersion(currentVersion), contract.object.abi)
+      data.contracts[contract.file][contract.name].abi = solcABI.update(this.truncateVersion(this.state.currentVersion), contract.object.abi)
     })
     return data
   }
 }
-
-module.exports = Compiler
