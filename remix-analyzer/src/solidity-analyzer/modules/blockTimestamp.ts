3c88c68e25c1f7aa6b83ee7f61976ef8a5752a38
aniket-engg <aniketengg.225@gmail.com>
2020-05-25 18:51:18 +0530

    warning links reviewed

--- remix-analyzer/src/solidity-analyzer/modules/blockTimestamp.ts
+++ remix-analyzer/src/solidity-analyzer/modules/blockTimestamp.ts
@@ -23,14 +23,14 @@ export default class blockTimestamp implements AnalyzerModule {
         warning: `use of "now": "now" does not mean current time. Now is an alias for block.timestamp. 
                   Block.timestamp can be influenced by miners to a certain degree, be careful.`,
         location: item.src,
-        more: 'http://solidity.readthedocs.io/en/develop/frequently-asked-questions.html#are-timestamps-now-block-timestamp-reliable'
+        more: 'https://solidity.readthedocs.io/en/develop/units-and-global-variables.html?highlight=block.timestamp#block-and-transaction-properties'
       }
     }).concat(this.warningblockTimestampNodes.map((item, i) => {
       return {
         warning: `use of "block.timestamp": "block.timestamp" can be influenced by miners to a certain degree. 
                   That means that a miner can "choose" the block.timestamp, to a certain degree, to change the outcome of a transaction in the mined block.`,
         location: item.src,
-        more: 'http://solidity.readthedocs.io/en/develop/frequently-asked-questions.html#are-timestamps-now-block-timestamp-reliable'
+        more: 'https://solidity.readthedocs.io/en/develop/units-and-global-variables.html?highlight=block.timestamp#block-and-transaction-properties'
       }
     }))
   }
